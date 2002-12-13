5761068fa5a9109b855330cdc4a71062c63a34e1
mrenold <mrenold@e2105ad2-bfd1-0310-a60b-926b18fec9b7>
2002-12-13 17:18:10 +0000

    Added easy install script.

---
+++ trunk/reinstall.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ ! -d ~/elisp ] ; then
+  echo "Put those things into ~/elisp !"
+  exit 1
+fi
+
+echo "This will delete your ~/.emacs and your ~/.viper!"
+echo "Ctrl-C abbort, RETURN continue."
+read i
+
+# well, he has been warned.
+cd ~
+rm .emacs .viper
+ln -s elisp/emacs .emacs
+ln -s elisp/viper .viper
+
+echo "Symlinks done."
+
