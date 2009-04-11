01f38be4f375859e11f0b2718f1dda54f09328e2
Martin Renold <martinxyz@gmx.ch>
2009-04-11 21:14:25 +0200

    backup cronjob

---
+++ scripts/backup_martin.py
@@ -0,0 +1,18 @@
+# cronjob script to backup to USB sticks (need to be user-mountable)
+
+from os import system, path
+import rsyncbackup as rb
+targets = ['/backupstick', '/backupstick_sony']
+success = 0
+for target in targets:
+  system('mount ' + target + ' 2>/dev/null')
+  if not path.isdir(target + '/lost+found'):
+    continue
+  try:
+    rb.backup('/home/martin/', target + '/martin')
+  finally:
+    system('sync')
+  success += 1
+
+assert success > 0, 'No USB sticks plugged in for backup!'
+
