82b5a7966b894520467e9403626bf0c229f8382c
Martin Renold <martinxyz@gmx.ch>
2009-04-21 20:13:22 +0200

    misc stuff

--- elisp/emacs.el
+++ elisp/emacs.el
@@ -111,8 +111,8 @@
   (interactive)
   (c++-mode)
   ; indent with tabs only.
-  (setq c-basic-offset 8
-        tab-width 8
+  (setq c-basic-offset 4
+        tab-width 4
         indent-tabs-mode t)
   )
 
