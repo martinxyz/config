c73d31bba2f080f63730bfbdbb8dc5fd082ddc93
Martin Renold <martinxyz@gmx.ch>
2009-08-18 20:03:18 +0200

    misc

--- elisp/emacs.el
+++ elisp/emacs.el
@@ -135,6 +135,7 @@
   (c-set-style "gnu")
   (setq indent-tabs-mode nil)
   (setq show-trailing-whitespace t)
+  (setq tab-width 8)
   )
 
 ;; TODO: Im c-mode beim Laden einer Datei 
@@ -159,6 +160,9 @@
           auto-mode-alist))
 (setq auto-mode-alist 
       (cons '(".*/gimp.*/.*\\.[ch]$" . gimp-c-mode)
+          auto-mode-alist))
+(setq auto-mode-alist 
+      (cons '(".*/gtk+.*/.*\\.[ch]$" . gimp-c-mode)
           auto-mode-alist))
 (setq auto-mode-alist 
       (cons '(".*\\.mas$" . python-mode)
