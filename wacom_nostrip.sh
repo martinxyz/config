345c1c48fe06a12cb3d6143034068665bf598447
Martin Renold <martinxyz@gmx.ch>
2010-06-07 20:03:41 +0200

    .

---
+++ wacom_nostrip.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pad='Wacom Intuos3 6x8 pad'
+xsetwacom set "$pad" StripLUp None
+xsetwacom set "$pad" StripRUp None
+xsetwacom set "$pad" StripLDn None
+xsetwacom set "$pad" StripRDn None
+
