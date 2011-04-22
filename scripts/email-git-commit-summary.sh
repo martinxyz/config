ddeec1ad18912d98a1fa09ed3b7a903768fa7af4
Martin Renold <martinxyz@gmx.ch>
2011-04-22 12:24:39 +0200

    git config stuff

--- scripts/email-git-commit-summary.sh
+++ scripts/email-git-commit-summary.sh
@@ -16,12 +16,12 @@ git fetch origin
 
 echo "new commits (oldest first):"
 echo
-if git log --reverse --oneline $tag..origin/master ; then
+if git log --reverse --oneline $tag..origin/master . ; then
     echo
     #echo "details (oldest first):"
     #git log --reverse -p --stat $tag..origin/master
     echo "Active Authors:"
-    git shortlog -n -s $tag..origin/master
+    git shortlog -n -s $tag..origin/master .
 else
     echo
     echo "Hm, tag $tag does not exist yet?"
