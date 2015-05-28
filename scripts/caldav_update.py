6695bc2cb3f9cd0443f0e2d88d0db9c1f79ea488
Martin Renold <martinxyz@gmx.ch>
2015-05-28 15:55:49 +0200

    calendar auto-show script & caldav sync

---
+++ scripts/caldav_update.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+"""
+Personal script to convert caldav calendar into human-readable
+textfile (one event per line).
+
+The idea is to print a summary of upcoming events once per day
+to some xterm I just opened. This increases the chance that I
+stay aware of things, compared to bio-polling a webpage or app.
+"""
+import os, sys, time
+
+def fn(name):
+    return os.path.join(os.getenv('HOME'), '.caldav-remind', name)
+    
+if not os.path.exists(fn('account')):
+    sys.exit(0)
+url, user, pw = open(fn('account')).read().split()
+#print repr(url), repr(user), 'pw length', len(pw)
+
+# update the file even if there is an error
+with open(fn('content'), 'w') as f:
+    f.write('outdated\n')
+
+import caldav
+from datetime import date, datetime, timedelta
+import pytz
+import icalendar
+
+client = caldav.DAVClient(url, username=user, password=pw)
+principal = caldav.Principal(client)
+calendars = principal.calendars()
+assert len(calendars) == 1, 'only a single calendar supported, for now'
+calendar = calendars[0]
+
+show_days = 30
+results = calendar.date_search(
+        datetime.now().date()+timedelta(days=1),
+        datetime.now().date()+timedelta(days=show_days+1))
+
+content = ''
+for event in results:
+  events = str(event)  
+
+  cal = icalendar.Calendar.from_ical(event.data)
+  assert len(cal.subcomponents) == 1
+  event = cal.subcomponents[0]
+  dt = event['DTSTART'].dt
+  # too lazy to figure out locale stuff
+  s = 'So Mo Di Mi Do Fr Sa'.split()[int(dt.strftime('%w'))] + ' '
+  #s += dt.strftime('%a %d.%m. ')
+  s += dt.strftime('%d.%m. ').replace('.0', '.')
+  s = s.replace(' 0', '  ')
+  hour = dt.strftime('%R')
+  if hour != '00:00':
+      s += hour + ' '
+  s += event['SUMMARY']
+  for name, value in event.items():
+      if name in 'SUMMARY TRANSP UID'.split():
+          continue
+      if isinstance(value, basestring):
+          s += ', ' + value
+  content += s + '\n'
+
+if not content:
+    content = 'no upcoming events\n'
+
+# update the file even if there is an error
+with open(fn('content'), 'w') as f:
+    f.write(content)
+
--- scripts/calshow.py
+++ scripts/calshow.py
@@ -4,21 +4,36 @@
 interval = 60*60*9
 
 import os, sys, time
+
+# my old calendar: hand-edited text file, one event per line
 f = os.path.join(os.getenv('HOME'), 'notes', 'cal')
-if not os.path.exists(f):
-    sys.exit(0)
-if time.time() - os.path.getmtime(f) < interval:
-    sys.exit(0)
+if os.path.exists(f) and \
+   time.time() - os.path.getmtime(f) >= interval:
+    data = open(f).read()
+    data = data.split('\n---')[1]
+    lines = [s for s in data.split('\n')[1:] if s.strip()]
+    lines = lines[:3]
+    if ''.join(lines).strip():
+        # calendar not empty
+        print
+        for line in lines:
+            print line[:80]
+        print
+        os.utime(f, None) # touch
 
-data = open(f).read()
-data = data.split('\n---')[1]
-lines = [s for s in data.split('\n')[1:] if s.strip()]
-lines = lines[:3]
-if not ''.join(lines).strip():
-    sys.exit(0) # calendar empty
-print
-for line in lines:
-    print line[:80]
-print
-os.utime(f, None) # touch
-
+# my new calendar: read-only text file, synced via caldav
+def fn(name):
+    return os.path.join(os.getenv('HOME'), '.caldav-remind', name)
+if os.path.exists(fn('content')):
+    if not os.path.exists(fn('last-shown')) or \
+       time.time() - os.path.getmtime(fn('last-shown')) >= interval:
+        data = open(fn('content')).read()
+        lines = [s for s in data.split('\n') if s.strip()]
+        lines = lines[:3]
+        if ''.join(lines).strip():
+            # calendar not empty
+            print
+            for line in lines:
+                print line[:80]
+            print
+            with open(fn('last-shown'), 'w'): pass # touch or create
