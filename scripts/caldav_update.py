a4ec8f371360710a4e4eac03639748f15bfa47fc
Martin Renold <martinxyz@gmx.ch>
2015-07-04 13:24:51 +0200

    fetch calendar with webdav
    (python caldav lib uses a parser lib that throws errors on some timezone info)

--- scripts/caldav_update.py
+++ scripts/caldav_update.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Personal script to convert caldav calendar into human-readable
 textfile (one event per line).
@@ -14,60 +14,63 @@ def fn(name):
     
 if not os.path.exists(fn('account')):
     sys.exit(0)
-url, user, pw = open(fn('account')).read().split()
-#print repr(url), repr(user), 'pw length', len(pw)
+server, serverpath, user, pw = open(fn('account')).read().split()
 
 # update the file even if there is an error
 with open(fn('content'), 'w') as f:
     f.write('outdated\n')
 
-import caldav
-from datetime import date, datetime, timedelta
-import pytz
+import easywebdav
 import icalendar
+import datetime
 
-client = caldav.DAVClient(url, username=user, password=pw)
-principal = caldav.Principal(client)
-calendars = principal.calendars()
-assert len(calendars) == 1, 'only a single calendar supported, for now'
-calendar = calendars[0]
+webdav = easywebdav.connect(server, protocol='https', username=user, password=pw)
+with open(fn('calendar.ical'), 'w') as f:
+    webdav.download(serverpath, f)
 
-show_days = 30
-results = calendar.date_search(
-        datetime.now().date(),#+timedelta(days=1),
-        datetime.now().date()+timedelta(days=show_days+1))
+g = open(fn('calendar.ical'))
+gcal = icalendar.Calendar.from_ical(g.read())
+events = []
+for component in gcal.walk():
+    if component.name == "VEVENT":
+        dt = component.decoded('dtstart')
+        if isinstance(dt, datetime.datetime):
+            dt = dt.date()
+        events.append((dt, component))
+g.close()
 
-ical_results = []
-for event in results:
-  events = str(event)  
-  cal = icalendar.Calendar.from_ical(event.data)
-  assert len(cal.subcomponents) == 1
-  event = cal.subcomponents[0]
-  dt = event['DTSTART'].dt
-  ical_results.append((dt, event))
- 
+show_days = 16
+today = datetime.date.today()
+d_min = today#+datetime.timedelta(days=1),
+d_max = today+datetime.timedelta(days=show_days+1)
+
+
 content = ''
-for dt, event in sorted(ical_results):
-  # too lazy to figure out locale stuff
-  s = 'So Mo Di Mi Do Fr Sa'.split()[int(dt.strftime('%w'))] + ' '
-  #s += dt.strftime('%a %d.%m. ')
-  s += dt.strftime('%d.%m. ').replace('.0', '.')
-  s = s.replace(' 0', '  ')
-  hour = dt.strftime('%R')
-  if hour != '00:00':
-      s += hour + ' '
-  s += event['SUMMARY']
-  for name, value in event.items():
-      if name in 'SUMMARY TRANSP UID'.split():
-          continue
-      if isinstance(value, basestring):
-          s += ', ' + value
-  content += s + '\n'
+for dt, event in sorted(events, key=lambda x: x[0]):
+    if dt > d_max or dt < d_min:
+        continue
+    # too lazy to figure out locale stuff
+    s = 'So Mo Di Mi Do Fr Sa'.split()[int(dt.strftime('%w'))] + ' '
+    #s += dt.strftime('%a %d.%m. ')
+    s += dt.strftime('%d.%m. ').replace('.0', '.')
+    s = s.replace(' 0', '  ')
+    hour = dt.strftime('%R')
+    if hour != '00:00':
+        s += hour + ' '
+    s += event['SUMMARY']
+    for name, value in list(event.items()):
+        if name in 'SUMMARY TRANSP UID'.split():
+            continue
+        if isinstance(value, str):
+            s += ', ' + value
+    if len(s) > 79:
+        s = s[:79-3] + '...'
+    content += s + '\n'
 
 if not content:
     content = 'no upcoming events\n'
 
 # update the file even if there is an error
 with open(fn('content'), 'w') as f:
-    f.write(content.encode('utf-8'))
+    f.write(content)
 
