99b55fedbef61fd4bbb0249882be12382078648a
Martin Renold <martin@log2.ch>
2017-02-05 13:59:21 +0100

    rsyncbackup - chdir away

--- scripts/rsyncbackup.py
+++ scripts/rsyncbackup.py
@@ -24,23 +24,23 @@ system is idle. After some time it will start no matter what.
 
 # basic rsync command and flags
 rsync = 'rsync -az --delete-excluded --numeric-ids --exclude-from=excludes '
-ssh   = 'ssh -o BatchMode=yes '
+ssh = 'ssh -o BatchMode=yes '
 
 DAY = 24*60*60.0
 
 # here you describe how many snapshots to keep
 # each slot stands for a time period
-backuptimes = [ 
-    # a new backup will be made if the first slot is empty 
+backuptimes = [
+    # a new backup will be made if the first slot is empty
     # and the system is idle, or if both the first and the
     # second slot are empty
-    2  *DAY , # keep one backup between 0 and 2 days old
-    4  *DAY , # keep one backup between 2 and 4 days old
-    8  *DAY , # ...
-    16 *DAY , 
-    32 *DAY , 
-    64 *DAY ,
-    128*DAY ,
+    2   * DAY,  # keep one backup between 0 and 2 days old
+    4   * DAY,  # keep one backup between 2 and 4 days old
+    8   * DAY,  # ...
+    16  * DAY,
+    32  * DAY,
+    64  * DAY,
+    128 * DAY,
     ]
 
 #################################
@@ -65,7 +65,7 @@ def backup_age(filename):
         return result
     except ValueError:
         return None
-    
+
 def scan_old_backups():
     "returns tuple: (newest backup file, slot id of newest backup, list of directories to remove)"
     slots = len(backuptimes)*[None]
@@ -73,13 +73,14 @@ def scan_old_backups():
     now = time.time()
     for filename in os.listdir('.'):
         dt = backup_age(filename)
-        if dt is None: continue
-        #print 'Processing:', filename, 'dt =', dt
+        if dt is None:
+            continue
+        # print 'Processing:', filename, 'dt =', dt
         i = 0
         while i < len(slots) and dt > backuptimes[i]:
             i += 1
         if i == len(slots):
-            #print 'Deleting over-aged backup', filename
+            # print 'Deleting over-aged backup', filename
             delete.append(filename)
             continue
         if slots[i] is None:
@@ -87,14 +88,14 @@ def scan_old_backups():
         else:
             old_dt = backup_age(slots[i])
             # keep only the oldest one in a slot
-            #print 'slot conflict:', (slots[i], old_dt), 'vs', (filename, dt)
+            # print 'slot conflict:', (slots[i], old_dt), 'vs', (filename, dt)
             if old_dt > dt:
                 delete.append(filename)
             else:
                 delete.append(slots[i])
                 slots[i] = filename
     i = 0
-    #print slots
+    # print slots
     while i < len(slots) and slots[i] is None:
         i += 1
     if i == len(slots):
@@ -104,53 +105,58 @@ def scan_old_backups():
 
 def backup(src, backupdir, ping=None, force=False):
     assert os.path.exists(backupdir), 'Please the create "%s" yourself and set proper permissions' % backupdir
+    old_cwd = os.getcwd()
     os.chdir(backupdir)
-    assert os.path.exists('excludes'), 'Please create an "excludes" file in the backup directory. Suggested content:' + excludes_example_content
-    (lastbackup, needed, delete) = scan_old_backups()
-    if force:
-	print 'Forced'
-	needed = 2
-    if needed == 0:
-        #print 'Not needed.'
-        return True
-    if needed == 1:
-        # load average last 5 minutes
-        load = float(os.popen('uptime').read().split()[-2][:-1])
-        #print 'Load', load,
-        if (load > 0.0): 
-            #print '--> later.'
+    try:
+        assert os.path.exists('excludes'), 'Please create an "excludes" file in the backup directory. Suggested content:' + excludes_example_content
+        (lastbackup, needed, delete) = scan_old_backups()
+        if force:
+            print 'Forced'
+            needed = 2
+        if needed == 0:
+            # print 'Not needed.'
             return True
-        #print '--> starting.'
-    if needed > 1:
-        pass #print 'Too many emtpy slots, starting without checking load.'
-    if needed > 2:
-        print src
-        print 'WARNING: no recent backups found, maybe something is wrong!'
-        print '         (Three or more empty slots. Retrying...)'
+        if needed == 1:
+            # load average last 5 minutes
+            load = float(os.popen('uptime').read().split()[-2][:-1])
+            # print 'Load', load,
+            if (load > 0.0):
+                # print '--> later.'
+                return True
+            # print '--> starting.'
+        if needed > 1:
+            pass  # print 'Too many emtpy slots, starting without checking load.'
+        if needed > 2:
+            print src
+            print 'WARNING: no recent backups found, maybe something is wrong!'
+            print '         (Three or more empty slots. Retrying...)'
 
-    if ping and os.system('ping -q -c 1 ' + ping + ' >/dev/null') != 0:
-        #print ping, 'does not respond to ping, skipping'
-        return
+        if ping and os.system('ping -q -c 1 ' + ping + ' >/dev/null') != 0:
+            # print ping, 'does not respond to ping, skipping'
+            return
 
-    if not os.path.exists('next'):
-        os.system('mkdir next')
-    else:
-        print 'Continuing with existing next/ directory from interrupted or erroneous rsync.'
-    timestamp = time.strftime('%Y%m%d')
+        if not os.path.exists('next'):
+            os.system('mkdir next')
+        else:
+            print 'Continuing with existing next/ directory from interrupted or erroneous rsync.'
+        timestamp = time.strftime('%Y%m%d')
 
-    assert not '"' in ssh
-    command = rsync + '-e "'+ssh+'" ' + src + ' ./next/'
-    if lastbackup:
-        command += ' --link-dest=' + os.path.join(backupdir, lastbackup)
-    result = os.system(command)
-    if result != 0:
-        print src
-        print 'WARNING: rsync returned exit code', + os.WEXITSTATUS(result)
-        print '         the next/ directory might hold partial results'
-        return
-    #os.system('touch next') # rsync resets mtime (or ctime?... only one of them)
-    os.rename('next', timestamp)
+        assert not '"' in ssh
+        command = rsync + '-e "'+ssh+'" ' + src + ' ./next/'
+        if lastbackup:
+            command += ' --link-dest=' + os.path.join(backupdir, lastbackup)
+        result = os.system(command)
+        if result != 0:
+            print src
+            print 'WARNING: rsync returned exit code', + os.WEXITSTATUS(result)
+            print '         the next/ directory might hold partial results'
+            return
+        # os.system('touch next') # rsync resets mtime (or ctime?... only one of them)
+        os.rename('next', timestamp)
 
-    for filename in delete:
-        #print 'Deleting old backup', filename
-        assert os.system('rm -rf ' + filename) == 0
+        for filename in delete:
+            # print 'Deleting old backup', filename
+            assert os.system('rm -rf ' + filename) == 0
+
+    finally:
+        os.chdir(old_cwd)
