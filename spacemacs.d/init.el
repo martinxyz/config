fd82784fb4e6e5561082969d64905b118c2fcd37
Martin Renold <martin@log2.ch>
2017-09-09 09:36:45 +0200

    js2r-log-this

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -393,6 +393,8 @@ you should place your code here."
      ))
   (advice-add 'devdocs-do-search :override #'maxy-devdocs-do-search)
 
+  (spacemacs/set-leader-keys "op" 'js2r-log-this)  ; a bit easier than SPC m r l t
+
   ;(spacemacs/set-leader-keys "og" 'ggtags-find-tag)
   (define-key evil-normal-state-map "Ω" 'evil-record-macro) ; Shift-@
 
