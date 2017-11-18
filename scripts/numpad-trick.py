766591989103a137cd6f2014d2b3f9db096ebf53
Martin Renold <martin@log2.ch>
2017-11-18 12:19:03 +0100

    numpad trick: volume control using key press duration

--- scripts/numpad-trick.py
+++ scripts/numpad-trick.py
@@ -12,19 +12,48 @@ device.grab()  # prevent X11 from getting the events, too
 mplaylist = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mplaylist.connect(('localhost', 28443))
 
+active_keycodes = set()
 
-async def print_events():
+
+def volume_keys(keyset):
+    plus = ecodes.KEY_KPPLUS in keyset
+    minus = ecodes.KEY_KPMINUS in keyset
+    if plus and minus:
+        return
+    elif plus:
+        subprocess.call(['amixer', 'sset', 'Master', '1%+'])
+    elif minus:
+        subprocess.call(['amixer', 'sset', 'Master', '1%-'])
+
+
+async def handle_active_keys():
+    while active_keycodes:
+        volume_keys(active_keycodes)
+        await asyncio.sleep(0.07)
+
+
+async def listen_to_numpad():
+    active_key_task = None
     async for event in device.async_read_loop():
         if event.type != ecodes.EV_KEY:
             continue
-        if event.value == 1 or event.value == 2:
-            if event.code == ecodes.KEY_KPMINUS:
-                subprocess.call(['amixer', 'sset', 'Master', '1%-'])
-            if event.code == ecodes.KEY_KPPLUS:
-                subprocess.call(['amixer', 'sset', 'Master', '1%+'])
         if event.value == 2:  # repeated
             continue
+        if event.value == 0:  # released
+            active_keycodes.remove(event.code)
         if event.value == 1:  # pressed
+            active_keycodes.add(event.code)
+
+            # execute one instant action
+            #
+            # When handle_active_keys() runs the key may already be released
+            # again, because the key handler runs before the coroutine we start
+            # right here... silly... this probably would have just worked
+            # if it followed Javascript's Promises/A+ spec...
+            volume_keys([event.code])
+            if not active_key_task or active_key_task.done():
+                active_key_task = asyncio.ensure_future(handle_active_keys())
+
             if event.code == ecodes.KEY_KPENTER:
                 mplaylist.send(b'\n')
             if event.code == ecodes.KEY_KP1:
@@ -36,8 +65,8 @@ async def print_events():
         if event.code == ecodes.KEY_NUMLOCK:
             continue
         print(evdev.categorize(event))
-        # print(event.value) # 0 or 1
+
 
 loop = asyncio.get_event_loop()
-asyncio.ensure_future(print_events())
+asyncio.ensure_future(listen_to_numpad())
 loop.run_forever()
