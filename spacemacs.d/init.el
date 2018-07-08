bc0897ff13f82da9eda42c80bc51e153b14ebf34
Martin Renold <martin@log2.ch>
2018-07-08 12:45:04 +0200

    playing around with yasnippet

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -958,7 +958,10 @@ This function is called at the very end of Spacemacs initialization."
  '(vc-annotate-very-old-color nil)
  '(whitespace-style
    (quote
-    (face tabs space-before-tab::tab space-before-tab tab-mark))))
+    (face tabs space-before-tab::tab space-before-tab tab-mark)))
+ '(yas-snippet-dirs
+   (quote
+    ("~/.spacemacs.d/snippets" "/home/martin/.emacs.d/snippets" yasnippet-snippets-dir))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
