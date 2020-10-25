a275f341ee0c90f2c01f588c629177eb497b451a
Martin Renold <martin@log2.ch>
2020-10-25 15:00:32 +0100

    calshow py3, stderr

--- scripts/calshow.py
+++ scripts/calshow.py
@@ -5,23 +5,7 @@ interval = 60*60*9
 
 import os, sys, time
 
-# my old calendar: hand-edited text file, one event per line
-f = os.path.join(os.getenv('HOME'), 'notes', 'cal')
-if os.path.exists(f) and \
-   time.time() - os.path.getmtime(f) >= interval:
-    data = open(f).read()
-    data = data.split('\n---')[1]
-    lines = [s for s in data.split('\n')[1:] if s.strip()]
-    lines = lines[:3]
-    if ''.join(lines).strip():
-        # calendar not empty
-        print()
-        for line in lines:
-            print(line[:80])
-        print()
-        os.utime(f, None) # touch
-
-# my new calendar: read-only text file, synced via caldav
+# read-only text file, synced via caldav
 def fn(name):
     return os.path.join(os.getenv('HOME'), '.caldav-remind', name)
 if os.path.exists(fn('content')):
@@ -32,8 +16,8 @@ if os.path.exists(fn('content')):
         lines = lines[:3]
         if ''.join(lines).strip():
             # calendar not empty
-            print()
+            print(file=sys.stderr)
             for line in lines:
-                print(line[:80])
-            print()
+                print(line[:80], file=sys.stderr)
+            print(file=sys.stderr)
             with open(fn('last-shown'), 'w'): pass # touch or create
